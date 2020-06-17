ea760749648583af19bba782a1efffc8db8e6a75
Haya <max9173627@gmail.com>
2020-06-17 13:05:25 +0900

    [add]サイコロを振った時のSEを追加

--- app/src/main/java/com/haya/diceapp/MainActivity.java
+++ app/src/main/java/com/haya/diceapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.haya.diceapp;
 
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ImageView Dice_1 = findViewById(R.id.diceImage1);
+        final ImageView Dice_2 = findViewById(R.id.diceImage2);
+
+        final int [] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6,};
+
         Button roll_Dice_btn = findViewById(R.id.rollDice);
+
+        final MediaPlayer mp = MediaPlayer.create(this,R.raw.dice_sound);
+
         roll_Dice_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("クリックテスト", "出力確認");
+
+            Random random = new Random();
+            int myRandomNumber = random.nextInt(6);
+            Dice_1.setImageResource(diceImages[myRandomNumber]);
+
+            myRandomNumber = random.nextInt(6);
+            Dice_2.setImageResource(diceImages[myRandomNumber]);
+
+            mp.start();
+
             }
         });
     }
